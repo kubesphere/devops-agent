842051634534c3ed1329757787abde55dc8991b1
stoneshi-yunify <stoneshi@kubesphere.io>
2025-02-11 15:14:53 +0800

    update binary ks
    Signed-off-by: stoneshi-yunify <stoneshi@kubesphere.io>

--- base/hack/install_utils.sh
+++ base/hack/install_utils.sh
@@ -4,70 +4,10 @@ ARCH=$(uname -m)
 
 echo $ARCH
 
-if [[ ${EXCLUDE_DOCKER} != '1' ]]; then
-  # Docker
-  DOCKER_VERSION=18.09.9
-  if [[ ${ARCH} == 'x86_64' ]]; then
-    curl -f https://download.docker.com/linux/static/stable/x86_64/docker-$DOCKER_VERSION.tgz | tar xvz && \
-    mv docker/docker /usr/bin/ && \
-    rm -rf docker
-  elif [[ ${ARCH} == 'aarch64' ]]
-  then
-    curl -f https://download.docker.com/linux/static/stable/aarch64/docker-$DOCKER_VERSION.tgz | tar xvz && \
-    mv docker/docker /usr/bin/ && \
-    rm -rf docker
-  else
-    echo "do not support this arch"
-    exit 1
-  fi
-fi
-
-# Helm
-HELM_VERSION=2.11.0
-HELM3_VERSIOIN=3.5.0
-
-if [[ ${ARCH} == 'x86_64' ]]; then
-  curl -f https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz  | tar xzv && \
-  mv linux-amd64/helm /usr/bin/ && \
-  mv linux-amd64/tiller /usr/bin/ && \
-  rm -rf linux-amd64
-
-  curl -f https://get.helm.sh/helm-v${HELM3_VERSIOIN}-linux-amd64.tar.gz | tar xzv && \
-  mv linux-amd64/helm /usr/bin/helm3 && \
-  rm -rf linux-amd64 
-elif [[ ${ARCH} == 'aarch64' ]]
-then
-  curl -f https://get.helm.sh/helm-v${HELM_VERSION}-linux-arm64.tar.gz  | tar xzv && \
-  mv linux-arm64/helm /usr/bin/ && \
-  mv linux-arm64/tiller /usr/bin/ && \
-  rm -rf linux-arm64
-
-  curl -f https://get.helm.sh/helm-v${HELM3_VERSIOIN}-linux-arm64.tar.gz | tar xzv && \
-  mv linux-arm64/helm /usr/bin/helm3 && \
-  rm -rf linux-arm64 
-else
-  echo "do not support this arch"
-  exit 1
-fi
-
-# kubectl
-
-if [[ ${ARCH} == 'x86_64' ]]; then
-  curl -f -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
-  chmod +x kubectl && \
-  mv kubectl /usr/bin/
-elif [[ ${ARCH} == 'aarch64' ]]
-then
-  curl -f -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/arm64/kubectl && \
-  chmod +x kubectl && \
-  mv kubectl /usr/bin/ && kubectl --help
-else
-  echo "do not support this arch"
-  exit 1
-fi
+rm -rf /usr/bin/ks
 
 # ks
-KS_VERSION=0.0.64
+KS_VERSION=0.0.72
 if [[ ${ARCH} == 'x86_64' ]]; then
   curl -fL https://github.com/kubesphere-sigs/ks/releases/download/v${KS_VERSION}/ks-linux-amd64.tar.gz | tar xzv && \
   mv ks /usr/bin/
@@ -75,20 +15,6 @@ elif [[ ${ARCH} == 'aarch64' ]]
 then
   curl -fL https://github.com/kubesphere-sigs/ks/releases/download/v${KS_VERSION}/ks-linux-arm64.tar.gz | tar xzv && \
   mv ks /usr/bin/
-else
-  echo "do not support this arch"
-  exit 1
-fi
-
-# kustomize
-KUSTOMIZE_VERSION=4.5.3
-if [[ ${ARCH} == 'x86_64' ]]; then
-  curl -fL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE_VERSION}/kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz | tar xzv && \
-  mv kustomize /usr/bin/
-elif [[ ${ARCH} == 'aarch64' ]]
-then
-  curl -fL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE_VERSION}/kustomize_v${KUSTOMIZE_VERSION}_linux_arm64.tar.gz | tar xzv && \
-  mv kustomize /usr/bin/
 else
   echo "do not support this arch"
   exit 1
